ec3a667cf37f3f1289f31388c08957a3d548bde4
Rafael <rafaelopires@icloud.com>
2023-10-18 23:20:28 -0300

    [Feature] Updated model

---
+++ LoginApp/Migrations/20231019002317_UserMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoginApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Vip",
+                table: "User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Vip",
+                table: "User");
+        }
+    }
+}
--- LoginApp/Models/User.cs
+++ LoginApp/Models/User.cs
@@ -6,5 +6,6 @@ namespace LoginApp.Models
         public string Email { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
+        public bool Vip { get; set; } = false;
     }
 }
