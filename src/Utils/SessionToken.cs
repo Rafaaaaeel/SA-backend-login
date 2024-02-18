43f4c3f5d89317b100e2d37903c91e81075b37d9
Rafael <rafaelopires@icloud.com>
2024-02-18 02:35:48 -0300

    Saving files

--- src/Utils/SessionToken.cs
+++ src/Utils/SessionToken.cs
@@ -2,18 +2,18 @@ namespace LoginApp.Utils;
 
 public class SessionToken : ISessionToken
 {
-    private readonly IConfiguration _configuration;
+    private readonly string _key;
 
-    public SessionToken(IConfiguration configuration)
+    public SessionToken(AppSettings appSettings)
     {
-        _configuration = configuration;
+        _key = appSettings.JwtConfiguration.IssuerSigningKey;
     }
 
     public string CreateToken(User user)
     {
         List<Claim> claims = [new(ClaimTypes.Email, user.Email)];
 
-        SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+        SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(_key));
 
         SigningCredentials creds = new(key, SecurityAlgorithms.HmacSha512Signature);
 
